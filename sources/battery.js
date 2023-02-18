ddb05ab3611c38f92c8db3c0b6287db2053ff4f5
Ossama9 <oussama.dahbi98@gmail.com>
2023-02-18 14:56:50 +0100

    Show battery

--- sources/battery.js
+++ sources/battery.js
@@ -1,41 +1,65 @@
-if ('getBattery' in navigator) {
-    navigator.getBattery().then(function (battery) {
-        const test = new Battery(battery)
-        console.log("getChargeLevel " + test.getChargeLevel())
-        console.log("getChargingTime " + test.getChargingTime())
-        console.log("isInCharge " + test.isInCharge())
-        console.log("getDisChargingTime " + test.getDisChargingTime())
-        test.getBatteryStatus()
-        console.log(battery)
-        // battery.addEventListener('chargingchange', function() {
-        //     if (battery.charging) {
-        //         console.log("La batterie est en train de se charger");
-        //     } else {
-        //         console.log("La batterie n'est pas en train de se charger");
-        //     }
-        // });
 
-        battery.addEventListener('chargingtimechange', function () {
-            console.log("Le temps de charge restant est de " + battery.chargingTime + " minutes");
+
+class BatteryManager {
+    constructor() {
+        this.battery = null;
+        this.batteryLevel = document.querySelector(".battery_level");
+        this.chargingBar = document.querySelector(".charging_bar");
+        this.chargingIcon = document.querySelector(".charging_icon");
+        this.otherInfo = document.querySelector(".other_info");
+    }
+
+
+    init() {
+        navigator.getBattery().then((battery) => {
+            this.battery = battery;
+            this.updateAllBatteryInfo();
+            this.addBatteryEventListeners();
+        });
+    }
+    updateAllBatteryInfo() {
+        this.updateChargeInfo();
+        this.updateLevelInfo();
+        this.updateDischargingInfo();
+    }
+    updateLevelInfo() {
+        this.batteryLevel.textContent = this.getChargeLevel();
+        this.chargingBar.style.width = this.getChargeLevel();
+    }
+    updateDischargingInfo() {
+        if (this.getDisChargingTime()) {
+            this.getDisChargingTime().textContent = `${parseInt(
+                this.getDisChargingTime()
+            )} minutes`;
+            this.otherInfo.style.display = "flex";
+        } else {
+            this.otherInfo.style.display = "none";
+        }
+    }
+    updateChargeInfo() {
+        if (this.isInCharge()) {
+            console.log(this.chargingBar)
+            this.chargingBar.style.animationIterationCount = "infinite";
+            this.chargingIcon.style.display = "inline-flex";
+            this.otherInfo.style.display = "none";
+        } else {
+            this.chargingIcon.style.display = "none";
+            this.otherInfo.style.display = "inline-flex";
+            this.chargingBar.style.animationIterationCount = "initial";
+        }
+    }
+    addBatteryEventListeners() {
+        this.battery.addEventListener("chargingchange", () => {
+            this.updateAllBatteryInfo();
         });
 
-        battery.addEventListener('dischargingtimechange', function () {
-            console.log("Le temps de décharge restant est de " + battery.dischargingTime + " minutes");
+        this.battery.addEventListener("levelchange", () => {
+            this.updateAllBatteryInfo();
         });
-    });
-} else {
-    console.log("L'option batterie n'est pas pris en charge par votre navigateur")
-}
 
-class Battery {
-    battery;
-    chargeLevel;
-    inCharge;
-    chargingTime;
-    disChargingTime;
-
-    constructor(battery) {
-        this.battery = battery;
+        this.battery.addEventListener("dischargingtimechange", () => {
+            this.updateAllBatteryInfo();
+        });
     }
 
     getChargeLevel() {
@@ -48,49 +72,15 @@ class Battery {
         return this.inCharge;
     }
 
-    // temps qui reste pour la batterie soit dechargé
-
-    /**
-     * temps qui reste pour la batterie soit chargé
-     * @returns {*}
-     */
-    getChargingTime() {
-        this.chargingTime = this.battery.chargingTime / 60;
-        if (this.chargingTime === Infinity) {
-            return -1
-        }
-        return this.chargingTime
-    }
-
-    /**
-     * temps qui reste pour la batterie soit dechargé
-     * @returns {*}
-     */
     getDisChargingTime() {
         this.disChargingTime = this.battery.dischargingTime / 60;
+        if (this.disChargingTime === Infinity)
+            return false
         return this.disChargingTime
     }
 
-    getBatteryStatus() {
-        this.battery.addEventListener('chargingchange', () => {
-            if (this.battery.charging) {
-                console.log("La batterie est en train de se charger");
-            } else {
-                console.log("La batterie n'est pas en train de se charger");
-            }
-        })
-        this.battery.addEventListener('chargingtimechange', () => {
-            if (this.isInCharge() === -1) {
-                console.log("Le temps de charge  test restant est de " + this.getChargingTime() + " minutes");
-            }
-        });
-        this.battery.addEventListener('dischargingtimechange', () => {
-            if (!this.isInCharge()) {
-                console.log("Le temps de decharge test restant est de " + this.getDisChargingTime() + " minutes");
-            }
-        });
-        this.battery.addEventListener('level', () => {
-            console.log("niveau de batterie est:" + this.getChargeLevel())
-        });
-    }
+
 }
+
+const batteryManager = new BatteryManager();
+batteryManager.init();
