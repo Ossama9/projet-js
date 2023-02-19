3fc0712437f3792379ceb1d1f27dc399868c0a5c
Ossama9 <oussama.dahbi98@gmail.com>
2023-02-19 10:50:54 +0100

    Fix imports

--- sources/assets/index.js
+++ sources/assets/index.js
@@ -1,4 +1,15 @@
 import {Clock} from "./Clock";
+import {BatteryManager} from "./battery";
+import {NetworkLatency} from "./NetworkLatency";
 
 const clock = new Clock();
+const battery = new BatteryManager();
+const networkLatency = new NetworkLatency()
+
+
+
+
+
+networkLatency.measurePeriodicLatency()
+battery.init();
 clock.start();
